a153b679060e721febac5a32b74db2227712520d
Ian Wong <wong.iancw@gmail.com>
2016-07-15 15:40:24 +1000

    more efficient algorithm

--- prob2/prob2.c
+++ prob2/prob2.c
@@ -26,11 +26,36 @@ int prob2_function(int limit) {
 	return sum;
 }
 
+// Method 2: Calculate every 3rd fibonnaci number, to avoid testing for 'even'
+// as every 3rd number is even! eg 1, 1, *2, 3, 5, *8, etc.
+// NOTE: The 3rd number is stored as 'nextfn'
+int prob2_function2(int limit) {
+	// initial fib numbers
+	int fn1 = 1;
+	int fn2 = 1;
+	int nextfn = fn1 + fn2;
+	int sum = 0;
+	while (nextfn < limit) {
+		sum += nextfn;
+		
+		// recalculate 3rd number
+		fn1 = fn2 + nextfn;
+		fn2 = nextfn + fn1;
+		nextfn = fn1 + fn2;
+	}
+	return sum;
+}
+
+// Method 3: Find by recursion, through the special recursive formula:
+// F(n)=4*F(n-3)+F(n-6)
+// where F(n) is an even fibonnaci number
+// NOTE: Similar to method 2, where we're checking every 3rd number
+
 int main(int argc, char* argv[]) {
 
 	// Return sum
 	printf("Sum of the even fibonnaci numbers less than 4 million:\n");
-	printf("%d\n",prob2_function(4000000));
+	printf("%d\n",prob2_function2(4000000));
 
 	return EXIT_SUCCESS;
 }
